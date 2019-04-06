1030dc422eb4716ea8e2fa7aa603f45698363211
Dmitry_Zhilenkov <zhildemon@gmail.com>
2019-04-06 13:00:24 +1000

    Connector to db
    How to connect to mysqldb with py

--- example.py
+++ example.py
@@ -1,1 +1,38 @@
 # Здесь был код
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 22:41:27 2019
+
+@author: Dmitry
+"""
+
+import mysql.connector
+from mysql.connector import Error
+ 
+ 
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='sakila',
+                                       user='root',
+                                       password='')
+        if conn.is_connected():
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM actor")
+            row = cursor.fetchone()
+            while row is not None:
+                print(row)
+                row = cursor.fetchone()
+#            print('Connected to MySQL database')
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        conn.close()
+ 
+ 
+if __name__ == '__main__':
+    connect()
