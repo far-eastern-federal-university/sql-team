95a2bcbf2ef91b16cc6744523041fd4aba581c68
Kloide <mostovayay@mail.ru>
2019-05-18 13:19:15 +1000

    Update! Mostovaia random data for DB

--- Mostovaia_Random.py
+++ Mostovaia_Random.py
@@ -4,24 +4,31 @@
 
 Это временный скриптовый файл.
 """
+
 import numpy as np
+import itertools
 #print(np.random.rand())
 #np.random.seed(123)
 namesM = ['Илья', 'Константин', 'Евгений', 'Алексей', 'Олег']
 namesF = ['Юлия', 'Александра', 'Екатерина', 'Елена', 'Мария']
 surnameM = ['Петров', 'Алексеев', 'Единорогов', 'Кеков', 'Горбатов']
 surnameF = ['Мостовая', 'Петрова', 'Мышкова', 'Самохина', 'Курочкина']
-listOfPpl = [[] for i in range(len(namesM)*4)]
+listOfPpl = []
 
-coin = np.random.sample()
+
 i = 0
-while i < len(namesM)*4:
-    if coin > 0.5 :
-        listOfPpl[i].append(namesM[np.random.randint(0, len(namesM))])
-        listOfPpl[i].append(surnameM[np.random.randint(0, len(surnameM))])
-        i += 1
-    else:
-        listOfPpl[i].append(namesF[np.random.randint(0, len(namesF))])
-        listOfPpl[i].append(surnameF[np.random.randint(0, len(surnameF))])    
-        i += 1
-print(listOfPpl)    
+for i in range(25):
+    coin = np.random.sample()
+    if coin > 0.5:
+        indN = np.random.randint(0, len(namesM))
+        indS = np.random.randint(0, len(surnameM))
+        listOfPpl.append([namesM[indN], surnameM[indS]])
+            
+    else:         
+        indN = np.random.randint(0, len(namesF))
+        indS = np.random.randint(0, len(surnameF))
+        listOfPpl.append([namesF[indN], surnameF[indS]])
+print(listOfPpl,'\n')     
+myset = set(tuple(x) for x in listOfPpl)
+listOfPpl =[list(x) for x in myset]
+print(listOfPpl   
