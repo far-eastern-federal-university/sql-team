ca74eec5f928127567fa039a1bb9b64311dc1e24
Kloide <mostovayay@mail.ru>
2019-06-23 19:28:55 +1000

    Updated Generator for Bookstore

--- Generator_Mostovaya_Bookstore.py
+++ Generator_Mostovaya_Bookstore.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Jun 14 20:45:01 2019
 
@@ -12,102 +11,134 @@ with open('books.csv', "r") as f:
     reader = csv.reader(f)
     your_list = list(reader)
 
-#print(your_list)
-
-Books = [your_list[i][0] for i in range(211)]
-Author_Name = list(set([your_list[i][1] for i in range(211)]))
-Author_Name.remove(' ')
-Author_Surname = [' Jaideva', ' John', ' Stephen', ' Edward', ' Vladimir', ' V P', ' Leonard', ' Frank', ' Maria', ' Gutierrez', ' Kurt', ' Cedric', ' Gerald', ' Abraham', ' Frank', ' John', ' Robert', ' H. G.', ' Werner', ' Andy', ' Terence', ' Drew', ' Nate', ' Wes', ' Thomas', ' Siddhartha', ' Albert', ' Arthur Conan', ' Arthur Conan', ' Adam', ' Ken', ' Adolf', ' Fritjof', ' Richard', 'Hemingway', ' Frederick', ' Jeffery', ' Randy', ' Ayn', ' Michael', 'Steinbeck', ' Edgar Allen', ' Stephen', ' Fritjof', ' Will', ' P L', ' John', ' John', ' John', ' John', ' V. S.', ' Joseph', ' Prime', ' Bob', ' Madan', ' Alfred', ' W. H.', ' Gary', ' Andrew', 'Forsyth', ' Schilling', ' Yashwant', ' Jonathan', ' Fyodor', ' Dan', ' Amartya', ' Amitav', ' Amartya', 'Hansberry', ' Bob', 'Archer', ' Kuldip', ' Sunita', ' William', ' Gill', ' P L', ' P L', ' P L', ' Sanjay', ' Johns', ' V P', ' Shashi', ' Shashi', ' Dominique', ' Dominique', ' Dominique', ' John', ' Bertrand', ' Sam', ' Shashi', ' Earle Stanley', ' Stephen', ' Peter', ' David', ' Victor', ' John', ' Peter', ' Richard', ' George', ' Amartya', ' William']
-Author_Surname = list(set(Author_Surname))
-Genre = [ 'nonfiction', 'data_science', 'science', 'mathematics', 'comic', 'signal_processing',  'economics', 'history', 'fiction', 'philosophy', 'computer_science', 'psychology']
-
-Publisher = ['New York Times' , 'Vintage', 'Wiley', 'Random House', 'Penguin', 'Fontana', 'Marinner Books', 'Bison Books', 'Love&Romance', 'Flowing', 'Limestine Press', 'Literary Guild']
-PublisherIND = [i for i in range(len(Publisher))]
-
-
-print(Year_Publish)
-
-List_Pub = []
-
-for i in range(len(Publisher)):    
-    
-    indY = np.random.randint(1890, 2018)
-    temp = Publisher[i]
-    List_Pub.append([temp, indY, PublisherIND[Publisher.index(temp)]])
-    
-print(List_Pub)    
-
 
 np.random.seed(542)
-
-ListOfBooks = []
-
-for i in range(1,211):
     
-    indB = np.random.randint(0, len(Books))
-    indN = np.random.randint(0, len(Author_Name))
-    indSE = np.random.randint(0, len(Author_Surname))
-    indP = np.random.randint(0, len(Publisher))
-    indG = np.random.randint(0, len(Genre))
-    ListOfBooks.append([Books[indB], Author_Name[indN],Author_Surname[indSE], Publisher[indP],Genre[indG], np.random.randint(1918, 2018), format(np.random.uniform(5,95), '.2f')])
-
+def Author():
+    Author_Name = list(set([your_list[i][1] for i in range(211)]))
+    Author_Name.remove(' ')
+    Author_Name.remove(' A Modern Approach')
+    Author_Surname = [' Jaideva', ' John', ' Stephen', ' Edward', ' Vladimir', ' V P', ' Leonard', ' Frank', ' Maria', ' Gutierrez', ' Kurt', ' Cedric', ' Gerald', ' Abraham', ' Frank', ' John', ' Robert', ' H. G.', ' Werner', ' Andy', ' Terence', ' Drew', ' Nate', ' Wes', ' Thomas', ' Siddhartha', ' Albert', ' Arthur Conan', ' Arthur Conan', ' Adam', ' Ken', ' Adolf', ' Fritjof', ' Richard', 'Hemingway', ' Frederick', ' Jeffery', ' Randy', ' Ayn', ' Michael', 'Steinbeck', ' Edgar Allen', ' Stephen', ' Fritjof', ' Will', ' P L', ' John', ' John', ' John', ' John', ' V. S.', ' Joseph', ' Prime', ' Bob', ' Madan', ' Alfred', ' W. H.', ' Gary', ' Andrew', 'Forsyth', ' Schilling', ' Yashwant', ' Jonathan', ' Fyodor', ' Dan', ' Amartya', ' Amitav', ' Amartya', 'Hansberry', ' Bob', 'Archer', ' Kuldip', ' Sunita', ' William', ' Gill', ' P L', ' P L', ' P L', ' Sanjay', ' Johns', ' V P', ' Shashi', ' Shashi', ' Dominique', ' Dominique', ' Dominique', ' John', ' Bertrand', ' Sam', ' Shashi', ' Earle Stanley', ' Stephen', ' Peter', ' David', ' Victor', ' John', ' Peter', ' Richard', ' George', ' Amartya', ' William']
+    Author_Surname = list(set(Author_Surname))
+    Author_List = []
+    for i in range(0,len(your_list)):
+         indN = np.random.randint(0, len(Author_Name))
+         indSE = np.random.randint(0, len(Author_Surname))
+         Author_List.append([Author_Name[indN], Author_Surname[indSE]])
+        
+    myset = set(tuple(x) for x in Author_List)
+    Author_List =sorted([list(x) for x in myset])   
     
+    for i in range(0, len(Author_List)):
+        Author_List[i].append(i)
+    return(Author_List)
 
-myset = set(tuple(x) for x in ListOfBooks)
+Author_Table = Author()   
+#print(len(Author_Table))  
 
-ListOfBooks =[list(x) for x in myset]
+def Publish():
+    Publisher = ['New York Times' , 'Vintage', 'Wiley', 'Random House', 'Penguin', 'Fontana', 
+                 'Marinner Books', 'Bison Books', 'Love&Romance', 'Flowing', 'Limestine Press', 
+                 'Literary Guild', 'HarperCollins', 'Simon & Schuster','Hachette Book Group',
+                 'Macmillan','Scholastic','Houghton Mifflin Harcourt','Workman','Sterling',
+                 'Abrams','Dover','Candlewick','W.W. Norton','Kensington','Chronicle','Sourcebooks',
+                 'B&H Publishing','Tyndale House','BloomsBury', 'London Press' ]
+    PublisherIND = [i for i in range(len(Publisher))]
+    List_Pub = []
+    for i in range(len(Publisher)):    
+    
+        indY = np.random.randint(1890, 2018)
+        temp = Publisher[i]
+        List_Pub.append([temp, indY, PublisherIND[Publisher.index(temp)]])
+        
+    return(List_Pub)    
 
-for x in range(len(ListOfBooks)):
-    for i in range(len(Publisher)):
-        if  ListOfBooks[x][3] == Publisher[i]:
-            ListOfBooks[x].insert(3, Publisher.index(ListOfBooks[x][3]))
-            ListOfBooks[x].remove(ListOfBooks[x][4])
+Publish_Table = Publish()
 
-#print(ListOfBooks) 
+#print(len(Publish_Table))    
 
-namesM = ['Ilya', 'Constantine', 'Alexander', 'Aleksei', 'Dmitriy', 'Stan', 'Oliver', 'Lee']
-namesF = ['Julia', 'Anastasia', 'Katherine', 'Irina', 'Maria', 'Deya', 'Freya', 'Lois', 'Gina']
-surnameM = ['Reserford', 'Vakarian', 'Shepard', 'Stark', 'Miller', 'Laslo', 'Elias', 'Lockhart']
-surnameF = ['Mostovaya', 'Petrova', 'Sozonova', 'Samohina', 'Lannister', 'Harllo', 'Piniata', 'Pier']
-Duty = ['cashier', 'janitor', 'warehouse worker', 'director']
-series = random.sample(range(1000,8900), 1000) 
-number =  number = random.sample(range(11000000000,89000000000),20)
-print(len(series), len(set(series)))
-ListOfStaff = []
+  
+    
+def books():
+    
+    Books = [your_list[i][0] for i in range(len(your_list))]
+    Genre = [ 'nonfiction', 'data_science', 'science', 'mathematics', 'comic', 'signal_processing',  'economics', 'history', 'fiction', 'philosophy', 'computer_science', 'psychology']    
+    ListOfBooks = []
+    for i in range(len(your_list)):
+    
+        indB = np.random.randint(0, len(Books))
+        ID_Author = np.random.randint(0, len(Author_Table))
+        indP = np.random.randint(0, len(Publish()))
+        indG = np.random.randint(0, len(Genre))
+        ListOfBooks.append([i, Books[indB],ID_Author, Publish()[indP][2], Genre[indG], np.random.randint(1918, 2018), format(np.random.uniform(5,95), '.2f')])
+    
+    myset = set(tuple(x) for x in ListOfBooks)
+    ListOfBooks =sorted([list(x) for x in myset]) 
+    return(ListOfBooks)
+    
+Books_Table = books()
 
-ListofCus = []
+#print(len(Books_Table))
 
-for i in range(20):
-    coin = np.random.sample()
-    if coin > 0.5:
- 
-        ListofCus.append([number[np.random.randint(0, 20)], surnameM[np.random.randint(0, len(surnameM))], namesM[np.random.randint(0, len(namesM))]])
+def phone():
+    n = str(random.randint(11000000000, 89000000000))
+    return n[:3] + '-' + n[3:6] + '-' + n[6:]
+
+def clients():
+    with open('Names.csv', "r") as f:
+        reader = csv.reader(f)
+        Names = list(reader)
+        
+    name = []  
+    
+    for i in range(0,200):
+        name.append([i, Names[i][1], Names[i][2], phone()])
+        
+    myset = set(tuple(x) for x in name)
+    name = sorted(([list(x) for x in myset])) 
+    return(name)
+    
+Clients_Table = clients()
+#print(len(Clients_Table))
+
+def OrderedBooks(): 
+    OrderBooks = []
+    bks = books()
+    BookID = [bks[x][0] for x in range(len(Books_Table))]
+    Price = []
+    for i in range(200):
+        
+        coin = np.random.sample()
+        
+        if coin <= 0.7:
+            a = (1,4)
+        else:
+            a = (5,10)
             
-    else:         
-
-        ListofCus.append([number[np.random.randint(0, 20)], surnameF[ np.random.randint(0, len(surnameF))], namesF[ np.random.randint(0, len(namesF))]])
-
-myset = set(tuple(x) for x in ListofCus)
-ListofCus =[list(x) for x in myset]   
-print(ListofCus)     
+        a = np.random.randint(*a)
     
-for i in range(8):
-    coin = np.random.sample()
-    if coin > 0.5:
-        indN = np.random.randint(0, len(namesM))
-        indS = np.random.randint(0, len(surnameM))
-        indSER = np.random.randint(0, len(series))
-        indDuty = np.random.randint(0, len(Duty))
-        ListOfStaff.append([series[indSER], surnameM[indS], namesM[indN], Duty[indDuty]])
+        listbook = [BookID[np.random.randint(len(Books_Table))] for i in range(a)]
+        cost = 0
+        for x in range(a):
+            OrderBooks.append([int(str(i)+str(listbook[x])),i, listbook[x]])
+            cost += float(Books_Table[listbook[x]][6])
+        Price.append([i, format(cost, '.2f')])
             
-    else:         
-        indN = np.random.randint(0, len(namesF))
-        indS = np.random.randint(0, len(surnameF))
-        indSE = np.random.randint(0, len(surnameF))
-        indSER = np.random.randint(0, len(series))
-        indDuty = np.random.randint(0, len(Duty))
-        ListOfStaff.append([series[indSER], surnameF[indS], namesF[indN], Duty[indDuty]])
+    return(OrderBooks, Price)       
+
+OrderedBooks_Table, Prices = OrderedBooks()
+
+#print(OrderedBooks_Table, '\n'*2, Prices)
+
+
+def Orders():
+    ClientID = random.sample(range(len(Clients_Table)), len(Clients_Table))
+    OrderID = list(set([OrderedBooks_Table[x][1] for x in range(len(OrderedBooks_Table))]))
+    Order = []
+    for i in range(len(OrderID)):
+        Order.append([OrderID[i], ClientID[i], float(Prices[i][1])])
+    return(Order)
+
+Order_Table = Orders()    
     
-myset = set(tuple(x) for x in ListOfStaff)
-ListOfStaff =[list(x) for x in myset]
-#print(ListOfStaff)  
+#print(len(Orders()))    
